31adc81a34922537fde594dc1817e7527e359ad9
jbirdjan <jbirdjan93@gmail.com>
2013-11-19 12:24:59 -0600

    Just empty shells of the instagram stuff

---
+++ src/com/se/cronus/items/instagram/InstagramItemFragmentControl.java
@@ -0,0 +1,27 @@
+package com.se.cronus.items.instagram;
+
+import android.view.View;
+
+import com.se.cronus.items.ItemFragmentCntrl;
+
+public class InstagramItemFragmentControl extends ItemFragmentCntrl {
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onLikeClick() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onCommentClick() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/com/se/cronus/items/instagram/InstagramItemFragmentView.java
@@ -0,0 +1,45 @@
+package com.se.cronus.items.instagram;
+
+import android.content.Context;
+
+import com.se.cronus.items.ItemDoc;
+import com.se.cronus.items.ItemFragmentView;
+
+public class InstagramItemFragmentView extends ItemFragmentView{
+
+	public InstagramItemFragmentView(int t, Context c) {
+		super(t, c);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onCreate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setLayoutParams() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean setDoc(ItemDoc d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ItemDoc getDoc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
